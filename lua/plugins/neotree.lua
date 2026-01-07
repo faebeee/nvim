acc9e3dc5c34488bcf13891eb208cb75093d466b
fabs <faebeee@gmail.com>
2026-01-07 10:23:45 +0100

    Init

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      hide_dotfiles = false,
+      window = {
+        mappings = {
+          ["<space>"] = "none",
+        },
+      },
+    },
+  },
+}
