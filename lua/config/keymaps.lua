55947e1b19ffa79c497b7c61ae00a20420e0508b
fabs <faebeee@gmail.com>
2026-01-16 13:25:40 +0100

    Improvements

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,6 +8,7 @@ vim.keymap.set("n", "<leader>E", ":Neotree reveal<CR>", { desc = "Explorer: Reve
 -- Load TypeScript/React specific configurations
 
 local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader><leader>", builtin.find_files, { desc = "Telescope find files" })
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
