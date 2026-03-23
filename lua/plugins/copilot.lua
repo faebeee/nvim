0096d4d5c62fccfe4de4e742a84dd806468705a8
fabs <faebeee@gmail.com>
2026-03-23 16:46:54 +0100

    Update copilot and add tailwind tools

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,53 +1,5 @@
 return {
   {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    opts = {
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        debounce = 75,
-        keymap = {
-          accept = "<C-r>",
-          accept_word = false,
-          accept_line = false,
-          next = "<M-]>",
-          prev = "<M-[>",
-          dismiss = "<C-]>",
-        },
-      },
-      panel = {
-        enabled = false,
-      },
-      filetypes = {
-        yaml = false,
-        markdown = false,
-        help = false,
-        gitcommit = false,
-        gitrebase = false,
-        hgcommit = false,
-        svn = false,
-        cvs = false,
-        ["."] = false,
-      },
-      copilot_node_command = vim.fn.executable("node") == 1 and "node" or nil,
-      server_opts_overrides = {
-        trace = "verbose",
-        settings = {
-          advanced = {
-            length_penalties = {
-              indentation = {
-                relative_empty_line_length = 2.0,
-                absolute_empty_line_length = 4,
-              },
-            },
-          },
-        },
-      },
-    },
-  },
-  {
     "CopilotC-Nvim/CopilotChat.nvim",
     branch = "canary",
     dependencies = {
