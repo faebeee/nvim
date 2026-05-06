25bee8238115b2810b36a27325545f3ef21bfc7c
fabs <faebeee@gmail.com>
2026-05-06 13:21:20 +0200

    Updates

---
+++ lua/plugins/mdx.lua
@@ -0,0 +1,29 @@
+return {
+  -- Treesitter grammar for MDX
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "markdown", "markdown_inline", "mdx" })
+    end,
+  },
+
+  -- mdx-analyzer LSP
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        mdx_analyzer = {},
+      },
+    },
+  },
+
+  -- Extend render-markdown to handle mdx filetype
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      file_types = { "markdown", "mdx" },
+    },
+    ft = { "markdown", "mdx" },
+  },
+}
