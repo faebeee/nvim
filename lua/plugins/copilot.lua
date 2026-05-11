52bcb9b8618d439f3e84f773e718d690128a2c9c
Fab <faebeee@gmail.com>
2026-05-11 17:20:23 +0200

    Remap key

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -9,7 +9,7 @@ return {
         auto_trigger = true,
         debounce = 75,
         keymap = {
-          accept = "<C-r>",
+          accept = "<C-e>",
           accept_word = false,
           accept_line = false,
           next = "<M-]>",
